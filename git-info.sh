e2ceafa8b510292edef4fc8340a0eaa6951f34ab
Karolin Varner <karo@cupdev.net>
2012-07-07 03:43:32 +0200

    Add a simple shellscript for gathering git info

---
+++ git-info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE="$1"
+
+for COMMIT in $(git rev-list HEAD -- $1 | tac); do
+    printf "commit-author "
+    git --no-pager show -s --format="%an" $COMMIT
+    
+    printf "commit-time "
+    git --no-pager show -s --format="%at" $COMMIT
+
+    printf "commit-message "
+    git --no-pager show -s --format="%s" $COMMIT
+done
